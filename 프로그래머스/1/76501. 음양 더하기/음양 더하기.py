7c0938b6553006409e1e48db15109f3eaf427071
Jun <javalalax@gmail.com>
2023-12-23 19:41:50 +0900

    Update 음양 더하기.py

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -6,3 +6,18 @@ def solution(absolutes, signs):
         else:
             answer -= num
     return answer
+
+
+'''
+처음 풀이: 딕셔너리 key로 value에 접근
+
+def solution(absolutes, signs):
+    answer = 0
+    total = dict(zip(absolutes, signs))
+    for num in total:
+        if total[num] == True:
+          answer += num
+        else:
+          answer -= num
+    return answer
+'''
