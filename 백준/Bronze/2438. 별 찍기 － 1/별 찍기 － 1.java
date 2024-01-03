4725739bce1d36f3dcd5ac9061169268898a0d6a
Jun <javalalax@gmail.com>
2024-01-03 22:34:48 +0900

    Update 별 찍기 － 1.java

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,10 +3,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    int tc = sc.nextInt();
-    for (int i=1; i<tc+1; i++) {
-        StringBuilder star = new StringBuilder("*");
-        System.out.println(star.toString().repeat(i));
+        int num = sc.nextInt();
+
+        for (int i=1; i<num+1; i++) {
+            String star = "*".repeat(i);
+            System.out.println(star);
+
+        /*
+        for (int i=1; i<tc+1; i++) {
+            StringBuilder star = new StringBuilder("*");
+            System.out.println(star.toString().repeat(i));
+        }
+        */
     }
-  }
-}
+} 
