9226decf43c149a028f4538ac2639143ab4d2df0
김지아(Kim Jia) <javalalax@gmail.com>
2024-08-02 15:22:37 +0900

    Update 행렬의 덧셈.java

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -5,7 +5,7 @@ class Solution {
         // 행렬의 덧셈 결과 반환 
         
         // 2차원 배열 선언 및 생성 
-        int[][] result = result = new int[arr1.length][arr1[0].length]; // 두 행렬의 행과 열의 크기가 같으므로 가능  
+        int[][] result = new int[arr1.length][arr1[0].length]; // 두 행렬의 행과 열의 크기가 같으므로 가능  
 
 		for (int i=0; i<arr1.length; i++) {
             for (int j=0; j<arr1[i].length; j++) {
