670e64d2c79cfa41b56b1ebf7f47594a85b4521c
김지아(Kim Jia) <watermoonxx@gmail.com>
2024-11-01 17:32:36 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int hp) {
+        
+        // 처음 작성 코드 
+        /*
+        int answer = 0;
+        
+        while (hp > 0) {
+            answer += hp / 5;
+            hp = hp % 5;
+            answer += hp / 3;
+            hp = hp % 3;
+            answer += hp / 1;
+            hp = hp % 1;
+        }    
+        return answer;
+        */
+        
+        // 수정 코드 
+        int answer = 0;
+        int[] ants = {5, 3, 1};
+        int i = 0;
+        
+        while (hp > 0) {
+            answer += hp / ants[i];
+            hp %= ants[i];
+            i++;
+        }
+        return answer;
+    }
+}
+
